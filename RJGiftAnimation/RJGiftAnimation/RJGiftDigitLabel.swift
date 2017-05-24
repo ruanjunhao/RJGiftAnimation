6ae4cff31e82743742f49ad7b9b2b5e30e30a33e
ruan037 <ruan037@126.com>
2017-05-24 10:21:10 +0800

    xx

---
+++ RJGiftAnimation/RJGiftAnimation/RJGiftDigitLabel.swift
@@ -0,0 +1,21 @@
+//
+//  RJGiftDigitLabel.swift
+//  RJGiftAnimation
+//
+//  Created by ruanjh on 2017/5/24.
+//  Copyright © 2017年 app. All rights reserved.
+//
+
+import UIKit
+
+class RJGiftDigitLabel: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
